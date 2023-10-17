51ca01b9eb5e47295f661805b769500b028e0dee
prats3992 <pratham3992@gmail.com>
2023-10-17 09:45:55 +0530

    Loading Page Finalised

--- js/script.js
+++ js/script.js
@@ -1,10 +1,7 @@
 // Simulate a delay for the loading page
 setTimeout(() => {
-    const loadingPage = document.querySelector('.loading-page');
-    loadingPage.style.display = 'none';
-
-    // Redirect to another URL after 5 seconds
-    setTimeout(() => {
-        window.location.href = 'index.html'; // Replace 'https://example.com' with the URL you want to open
-    }, 500); // 5000 milliseconds (5 seconds)
-}, 5000); // Initial delay, adjust as needed
+  // Redirect to another URL after 5 seconds
+  setTimeout(() => {
+    window.location.href = "index.html";
+  }, 500); // 5000 milliseconds (5 seconds)
+}, 3000); // Initial delay, adjust as needed
